1e4e10ab6193336dd45cd4871ae471b1b3b99387
Priyanshu <59302986+Priyanshu-CODERX@users.noreply.github.com>
2020-05-25 18:29:10 +0530

    Syntax Changes
    Here I have changed syntax of my code

--- Quotes.py
+++ Quotes.py
@@ -5,18 +5,22 @@ import requests
 import time
 
 def getQuotes(): # Fetching the quotes from Quotable's Database
-	response = requests.get('https://api.quotable.io/random')
+	url = 'https://api.quotable.io/random'
+	parameters = {'content' : 'famous-quotes'}
+	response = requests.get(url, params=parameters)
 	if response.status_code == 200:
 		quotes = response.json()
-		quote = 'f{quotes[content]})'
-		print(quotes)
+		print(quotes['content'])
+		print(quotes['author'])
 	else:
-		print("Failed to get a quote")
+		print("Sorry! Cannot get you a quote right now :( Please try again after sometime")
 
 def main():
 	try:
 		while True: # Automating the code
 			getQuotes()
+			print('__________________________________________________________')
+			print()
 			time.sleep(5)
 	except KeyboardInterrupt:
 		print("Thank You! Quitting")
